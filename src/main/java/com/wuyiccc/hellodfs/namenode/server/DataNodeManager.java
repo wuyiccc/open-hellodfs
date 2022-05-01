9ec037caa2b4c8cdaa3985c7eeaceae0415b6aa0
wuyiccc <3406324191@qq.com>
2022-05-01 20:48:27 +0800

    在Master节点中实现基于心跳机智的故障节点检测

--- src/main/java/com/wuyiccc/hellodfs/namenode/server/DataNodeManager.java
+++ src/main/java/com/wuyiccc/hellodfs/namenode/server/DataNodeManager.java
@@ -1,10 +1,11 @@
 package com.wuyiccc.hellodfs.namenode.server;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * manager datanode cluster
@@ -17,6 +18,10 @@ public class DataNodeManager {
     private Map<String, DataNodeInfo> dataNodeMap = new ConcurrentHashMap<>();
 
 
+    public DataNodeManager() {
+        new DataNodeAliveMonitor().start();
+    }
+
     public Boolean register(String ip, String hostname) {
         DataNodeInfo dataNodeInfo = new DataNodeInfo(ip, hostname);
         dataNodeMap.put(ip + "-" + hostname, dataNodeInfo);
@@ -25,8 +30,48 @@ public class DataNodeManager {
 
     public Boolean heartBeat(String ip, String hostname) {
         DataNodeInfo dataNodeInfo = dataNodeMap.get(ip + "-" + hostname);
-        dataNodeInfo.setLastHeartBeatTime(System.currentTimeMillis());
+        if (dataNodeInfo != null) {
+            dataNodeInfo.setLastHeartBeatTime(System.currentTimeMillis());
+        }
         return true;
+    }
+
+    /**
+     * check datanode is alive
+     */
+    class DataNodeAliveMonitor extends Thread {
+
+        @Override
+        public void run() {
+
+            while (true) {
+
+                try {
+                    List<String> toRemoveDataNodes = new ArrayList<>();
+
+                    Iterator<DataNodeInfo> dataNodeInfoIterator = dataNodeMap.values().iterator();
+                    DataNodeInfo dataNodeInfo = null;
+
+                    while (dataNodeInfoIterator.hasNext()) {
+                        dataNodeInfo = dataNodeInfoIterator.next();
+                        if (System.currentTimeMillis() - dataNodeInfo.getLastHeartBeatTime() > 90 * 1000) {
+                            toRemoveDataNodes.add(dataNodeInfo.getIp() + "-" + dataNodeInfo.getHostname());
+                        }
+                    }
+                    if (!toRemoveDataNodes.isEmpty()) {
+                        for (String toRemoveDataNode : toRemoveDataNodes) {
+                            dataNodeMap.remove(toRemoveDataNode);
+                        }
+                    }
+
+                    TimeUnit.SECONDS.sleep(30);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
     }
 
 }
