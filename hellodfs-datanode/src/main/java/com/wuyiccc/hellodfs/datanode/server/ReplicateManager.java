af7f384ae2846ca626d0dd7bedccce98d2199e33
wuyiccc <3406324191@qq.com>
2022-05-23 22:46:10 +0800

    设计用于异步复制的线程

--- hellodfs-datanode/src/main/java/com/wuyiccc/hellodfs/datanode/server/ReplicateManager.java
+++ hellodfs-datanode/src/main/java/com/wuyiccc/hellodfs/datanode/server/ReplicateManager.java
@@ -1,9 +1,9 @@
 package com.wuyiccc.hellodfs.datanode.server;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author wuyiccc
@@ -11,9 +11,37 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  */
 public class ReplicateManager {
 
+
+    public static final Integer REPLICATE_THREAD_NUM = 3;
+
     private ConcurrentLinkedQueue<JSONObject> replicateTaskQueue = new ConcurrentLinkedQueue<>();
+
+    public ReplicateManager() {
+        for (int i = 0; i < REPLICATE_THREAD_NUM; i++) {
+            new ReplicateWorker().start();
+        }
+    }
+
 
     public void addReplicateTask(JSONObject replicateTask) {
         replicateTaskQueue.offer(replicateTask);
+    }
+
+    class ReplicateWorker extends Thread {
+        @Override
+        public void run() {
+
+            while (true) {
+                try {
+                    JSONObject replicateTask = replicateTaskQueue.poll();
+                    if (replicateTask == null) {
+                        TimeUnit.SECONDS.sleep(1);
+                        continue;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
