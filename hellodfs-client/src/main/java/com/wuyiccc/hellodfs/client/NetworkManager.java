f1756cdf8fc286e85b9d2cedc8b3c8ab2ca1878b
wuyiccc <3406324191@qq.com>
2022-05-28 20:13:15 +0800

    在poll中完成网络连接的建立并放入缓存中

--- hellodfs-client/src/main/java/com/wuyiccc/hellodfs/client/NetworkManager.java
+++ hellodfs-client/src/main/java/com/wuyiccc/hellodfs/client/NetworkManager.java
@@ -5,9 +5,11 @@ import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author wuyiccc
@@ -26,23 +28,26 @@ public class NetworkManager {
      */
     public static final Integer CONNECTED = 2;
 
+    public static final Long POLL_TIMEOUT = 500L;
+
     private Selector selector;
+
+
+    /**
+     * A machine waiting to establish a connection
+     */
+    private ConcurrentLinkedQueue<Host> waitingConnectHosts;
 
     /**
      * all connect
      */
     private Map<String, SocketChannel> connections;
 
-
     /**
      * the status of each connection
      */
     private Map<String, Integer> connectState;
 
-    /**
-     * A machine waiting to establish a connection
-     */
-    private ConcurrentLinkedQueue<Host> waitingConnectHosts;
 
     public NetworkManager() {
         try {
@@ -62,42 +67,88 @@ public class NetworkManager {
      * Attempt to connect to the specified dataNode
      */
     public void maybeConnect(String hostname, Integer nioPort) throws Exception {
-        synchronized(this) {
-            if(!connectState.containsKey(hostname)) {
+        synchronized (this) {
+            if (!connectState.containsKey(hostname)) {
                 connectState.put(hostname, CONNECTING);
                 waitingConnectHosts.offer(new Host(hostname, nioPort));
             }
-            while(connectState.get(hostname).equals(CONNECTING)) {
+            while (connectState.get(hostname).equals(CONNECTING)) {
                 wait(100);
             }
         }
     }
 
-    /**
-     * Attempt to initiate a connection request from the queued machine
-     */
-    private void tryConnect() {
-        try {
-            Host host = null;
-            SocketChannel channel = null;
-
-            while((host = waitingConnectHosts.poll()) != null) {
-                channel = SocketChannel.open();
-                channel.configureBlocking(false);
-                channel.connect(new InetSocketAddress(host.hostname, host.nioPort));
-                channel.register(selector, SelectionKey.OP_CONNECT);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     class NetworkPollThread extends Thread {
 
         @Override
         public void run() {
-            while(true) {
+            while (true) {
                 tryConnect();
+                poll();
+            }
+        }
+
+        /**
+         * Attempt to initiate a connection request from the queued machine
+         */
+        private void tryConnect() {
+            try {
+                Host host = null;
+                SocketChannel channel = null;
+
+                while ((host = waitingConnectHosts.poll()) != null) {
+                    channel = SocketChannel.open();
+                    channel.configureBlocking(false);
+                    channel.connect(new InetSocketAddress(host.hostname, host.nioPort));
+                    channel.register(selector, SelectionKey.OP_CONNECT);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void poll() {
+            SocketChannel channel = null;
+
+            try {
+                // 500ms timeout
+                int selectedKeys = selector.select(POLL_TIMEOUT);
+                if (selectedKeys <= 0) {
+                    return;
+                }
+
+                Iterator<SelectionKey> keysIterator = selector.selectedKeys().iterator();
+                while (keysIterator.hasNext()) {
+                    SelectionKey key = keysIterator.next();
+                    keysIterator.remove();
+
+                    if (key.isConnectable()) {
+                        channel = (SocketChannel) key.channel();
+                        if (channel.isConnectionPending()) {
+                            while (!channel.finishConnect()) {
+                                TimeUnit.MILLISECONDS.sleep(100);
+                            }
+                        }
+
+                        System.out.println("completed connect with server......");
+
+                        InetSocketAddress remoteAddress = (InetSocketAddress) channel.getRemoteAddress();
+                        // refresh cache
+                        connectState.put(remoteAddress.getHostName(), CONNECTED);
+                        connections.put(remoteAddress.getHostName(), channel);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                if (channel != null) {
+                    try {
+                        channel.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
             }
         }
 
