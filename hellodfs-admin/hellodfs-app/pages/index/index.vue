f44c1c68e773a556cbd74bb71e44b291d2577413
wuyiccc <3406324191@qq.com>
2022-05-31 07:15:40 +0800

    首页导航栏1

--- hellodfs-admin/hellodfs-app/pages/index/index.vue
+++ hellodfs-admin/hellodfs-app/pages/index/index.vue
@@ -1,12 +1,38 @@
 <template>
-	<view class="text-main">
-    <text class="iconfont icon-file-b-" style="font-size: 100rpx;"></text>
+	<view>
+    <view>
+      <!--应用状态栏-->
+      <uni-status-bar></uni-status-bar>
+
+      <view style="height: 44px;" class="flex border-bottom align-center">
+
+        <view class="flex-1 flex">
+          <text class="font-md ml-3">首页</text>
+        </view>
+
+        <view class="flex-1 flex justify-center"></view>
+
+        <view class="flex-1 flex justify-end">
+          <view style="width: 60rpx; height: 60rpx" class="flex align-center justify-center bg-light rounded-circle mr-3">
+            <text class="iconfont icon-zengjia"></text>
+          </view>
+          <view style="width: 60rpx; height: 60rpx" class="flex align-center justify-center bg-light rounded-circle mr-3">
+            <text class="iconfont icon-gengduo"></text>
+          </view>
+        </view>
+
+      </view>
+
+    </view>
 	</view>
 </template>
 
 <script>
+import UniStatusBar from "../../components/uni-ui/uni-status-bar/uni-status-bar";
+
 	export default {
-		data() {
+    components: {UniStatusBar},
+    data() {
 			return {
 			}
 		},
