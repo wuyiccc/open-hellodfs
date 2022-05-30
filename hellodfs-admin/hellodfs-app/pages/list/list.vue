ab9d57dfac6b310c0f3347f5fe4c1522cbd8ba3e
wuyiccc <3406324191@qq.com>
2022-05-30 23:33:49 +0800

    配置tabbar底部导航

---
+++ hellodfs-admin/hellodfs-app/pages/list/list.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    传输列表
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ hellodfs-admin/hellodfs-app/pages/my/my.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    我的个人中心
+  </view>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
