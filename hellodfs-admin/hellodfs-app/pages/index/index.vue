73c6a903f6b22ad208bcd76d74900652e0be6e52
wuyiccc <3406324191@qq.com>
2022-05-31 23:25:24 +0800

    搜索栏美化

--- hellodfs-admin/hellodfs-app/pages/index/index.vue
+++ hellodfs-admin/hellodfs-app/pages/index/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<view>
+
+    <!--导航栏-->
     <nav-bar>
-
       <text slot="left" class="font-md ml-3">首页</text>
-
       <template slot="right">
         <view style="width: 60rpx; height: 60rpx" class="flex align-center justify-center bg-light rounded-circle mr-3">
           <text class="iconfont icon-zengjia"></text>
@@ -12,10 +12,18 @@
           <text class="iconfont icon-gengduo"></text>
         </view>
       </template>
-
     </nav-bar>
 
-    <view class="bg-danger" style="height: 1000px;">123</view>
+    <view class="px-3 py-2">
+      <view class="position-relative">
+        <view style="height: 70rpx; width: 70rpx; position: absolute; top: 0; left: 0;" class="flex align-center justify-center">
+          <text class="iconfont icon-sousuo text-light-muted"></text>
+        </view>
+        <input style="height: 70rpx; padding-left: 70rpx;" type="text" class="bg-light font-md rounded-circle" placeholder="搜索hellodfs文件"/>
+      </view>
+    </view>
+
+
 	</view>
 </template>
 
