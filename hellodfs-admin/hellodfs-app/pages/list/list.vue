cb39f1abac0d0679d4f9e6d7f6a59aabc65f4fce
wuyiccc <3406324191@qq.com>
2022-06-05 11:41:27 +0800

    传输列表1

--- hellodfs-admin/hellodfs-app/pages/list/list.vue
+++ hellodfs-admin/hellodfs-app/pages/list/list.vue
@@ -1,6 +1,11 @@
 <template>
-  <view>
-    传输列表
+  <view style="height: 100vh;" class="flex flex-column">
+    <view class="flex border-bottom border-light-secondary" style="height: 100rpx;">
+      <view class="flex-1 flex flex-column align-center justify-center " v-for="(item,index) in tabBars" :key="index" :class="index === tabIndex ? 'text-main' : ''" @click="changeTab(index)">
+        <text class="font-md">{{item.name}}</text>
+        <text style="height: 8rpx;width: 30rpx;" class="rounded" :class="tabIndex === index ? 'bg-main' : 'bg-white'"></text>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,11 +13,18 @@
 export default {
   data() {
     return {
-
+      tabIndex:0,
+      tabBars:[{
+        name:"下载列表"
+      },{
+        name:"上传列表"
+      }]
     }
   },
   methods: {
-
+    changeTab(index){
+      this.tabIndex = index
+    }
   }
 }
 </script>
