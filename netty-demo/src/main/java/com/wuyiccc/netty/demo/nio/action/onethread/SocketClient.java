a38c6c9c0b27fcfc7c256cbc38ff109c11ccb188
wuyiccc <3406324191@qq.com>
2022-07-24 16:03:28 +0800

    单线程socket

---
+++ netty-demo/src/main/java/com/wuyiccc/netty/demo/nio/action/onethread/SocketClient.java
@@ -0,0 +1,34 @@
+package com.wuyiccc.netty.demo.nio.action.onethread;
+
+import javax.xml.soap.SAAJMetaFactory;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * @author wuyiccc
+ * @date 2022/6/26 15:40
+ */
+public class SocketClient {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 9000);
+        InputStreamReader in = new InputStreamReader(socket.getInputStream());
+        OutputStream out = socket.getOutputStream();
+
+        out.write("你好".getBytes());
+        char[] buf = new char[1024 * 1024];
+        int len = in.read(buf);
+
+        while (len != -1) {
+            String response = new String(buf, 0, len);
+            System.out.println("客户端收到了响应: " + response);
+            len = in.read(buf);
+        }
+
+        in.close();
+        out.close();
+        socket.close();
+    }
+}
